84d4faca41ec0ff5d8b1aab6d8fb5123bde68c49
Negrativo <lucas.rsouza2000@gmail.com>
2021-10-15 20:05:32 -0300

    Adicionado maps a tela de pesquisa

--- src/pages/principal/Pesquisa.js
+++ src/pages/principal/Pesquisa.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { View, Image, TextInput, FlatList } from 'react-native';
+import MapView from 'react-native-maps';
 
 import iconPesquisa from '../../../assets/pesquisar.png';
 import Styles from '../../Styles/StylesAbasPrincipais';
 import api from '../../services/api';
 import OpcoesComponets from '../../Componentes/pesquisaOpcoes';
+import useLocation from '../../Componentes/maps/UserLocation';
 
 export default function({ navigation }) {
-    const[Dados, setDados] = useState('')
+    const [Dados, setDados] = useState('');
+    const [ErrorMsg, setErrorMsg] = useState(null);
+    const [Coords, setCoords] = useState(null); 
+     
+    const [latitude, setLatitude] = useState(-20.398259);	
+    const [longitude, setLongitude] = useState(-43.507726);
+    const { coords, errorMsg } = useLocation();
 
     useEffect(() => {
         api.get('/pesquisa/cargos', {})
@@ -38,13 +46,31 @@ export default function({ navigation }) {
                     showsVerticalScrollIndicator={false}
                     data={Dados}
                     keyExtractor={(item, Dados) => Dados.toString()}
-                    renderItem={({item}) => {
-                        return (
-                            <OpcoesComponets cargo={item.nome}/>
-                        )
-                    }}
+                    //renderItem={({item}) => {
+                      //  return (
+                       //     <OpcoesComponets cargo={item.nome}/>
+                        //)
+                    //}}
                 />
             </View>
+            <MapView
+                showsUserLocation={true}
+     	        showsMyLocationButton={false}
+                toolbarEnabled={false}
+                style={{
+                    height: '100%',
+                    width: '100%',
+                    position: 'relative',		
+              }}
+              initialRegion={{
+                latitude,	
+                longitude,	
+                latitudeDelta: 0.195,  	
+                longitudeDelta: 0.1921,
+                latitude, //...coords	// Aqui sobrescrevemos as variáveis latitude e longitude com a posição do usuário obtida no UserLocation que criamos para obter a localização.
+                longitude
+              }}
+            />
         </View>
     );
 
